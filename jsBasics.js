3fae90f435b71e7df47caea22d5c959093e34069
mlo-olagunju <oladele@skillupafrica.com.ng>
2023-12-05 12:42:31 +0100

    learning javascript basics

---
+++ jsBasics.js
@@ -0,0 +1,13 @@
+// alert('hello world!');
+
+// console.log('hello world');
+
+// variables
+
+let greeting = 'hello skillup';
+
+// console.log(greeting);
+
+let skillup = 1;
+
+// let, const, var
