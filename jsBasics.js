e630c5e243ac23b92d2ff49873843504d48fcdb9
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-07 10:42:15 +0100

    learning js

--- jsBasics.js
+++ jsBasics.js
@@ -4,10 +4,36 @@
 
 // variables
 
-let greeting = 'hello skillup';
+// let greeting = 'hello skillup';
 
 // console.log(greeting);
 
-let skillup = 1;
+// let skillup = 1;
 
-// let, const, var
+// // let, const, var
+
+// function add(number1, number2) {
+//   let sum = number1 + number2;
+//   return "sum";
+// }
+
+// add(3, 5);
+
+// function greet() {
+//   return "hello world";
+//   console.log("hello world");
+// }
+
+greet();
+
+// arrow function
+const greeting = () => {
+  console.log("good morning");
+  return "good morning";
+};
+
+console.log(greetings());
+
+// class function name(params) {
+
+// }
