c7d5336795afcdbbd4fc8874361e1b489ce606c2
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-12 11:49:32 +0100

    learning js arrays and arrays method

--- jsBasics.js
+++ jsBasics.js
@@ -27,10 +27,10 @@ function greet() {
 greet();
 */
 // arrow function
-const greeting = () => {
-  console.log("good morning");
-  return "good morning";
-};
+// const greeting = () => {
+//   console.log("good morning");
+//   return "good morning";
+// };
 
 // console.log(greetings());
 
@@ -38,44 +38,44 @@ const greeting = () => {
 
 // }
 
-function add3(numb1, numb2, numb3) {
-  let sum = numb1 + numb2 + numb3;
-  console.log(sum);
-}
+// function add3(numb1, numb2, numb3) {
+//   let sum = numb1 + numb2 + numb3;
+//   console.log(sum);
+// }
 
-add3(2, 2, 2);
+// add3(2, 2, 2);
 
-const addition = (para1, para2, para3) => {
-  let total = para1 + para2 + para3;
-  return total;
-};
+// const addition = (para1, para2, para3) => {
+//   let total = para1 + para2 + para3;
+//   return total;
+// };
 
-addition(2, 4, 6);
-console.log(addition(2, 4, 6));
+// addition(2, 4, 6);
+// console.log(addition(2, 4, 6));
 
-// block scope
-// global scope
-// let
-// let variable_name = value;
-//var function scoped global scope
+// // block scope
+// // global scope
+// // let
+// // let variable_name = value;
+// //var function scoped global scope
 
-// var numbers = 20;
-// var numbers = 50;
+// // var numbers = 20;
+// // var numbers = 50;
 
-var x = 12;
-x = 13;
-x += 1;
-console.log(x);
+// var x = 12;
+// x = 13;
+// x += 1;
+// console.log(x);
 
-//object
-const person = {
-  first_name: "Manny",
-  second_name: "Ladrich",
-  age: 20,
-  about: "web developer",
-};
+// //object
+// const person = {
+//   first_name: "Manny",
+//   second_name: "Ladrich",
+//   age: 20,
+//   about: "web developer",
+// };
 
-console.log(person.first_name);
+// console.log(person.first_name);
 
 //Data types
 //==========================================================================
@@ -86,6 +86,7 @@ console.log(person.first_name);
 // object = {}
 
 //declaring an array------
+/*
 const arrayName = [
   "Lorem",
   "Ipsum",
@@ -98,15 +99,32 @@ const arrayName = [
   "Lorem",
   "Ipsum",
 ];
+*/
 // const arrayName = new Array ('Lorem','Ipsum','Lorem','Ipsum','Lorem','Ipsum','Lorem','Ipsum','Lorem','Ipsum')
 
-console.log(arrayName);
+// console.log(arrayName);
 
-const numbers = [1, 2, 3, 4, 5, 6];
+// const numbers = [1, 2, 3, 4, 5, 6];
 
-//.push adds a value to the end of the array list
-//.pop removes the last value of the array list
-//.shift removes the first value of the array list
+//.push() adds a new value to the end of the array list (takes parameter asin: push(30) .... i.e 30 is the  parameter)
+//.pop() removes the last value of the array list (doesn't take parameter)
+//.shift() removes the first value of the array list (doesn't take parameter)
+//.unshift() adds a new value to the beginning of the array list (takes parameter asin: unshift(30) .... i.e 30 is the  parameter)
+// splice()
+// slice()
 
-numbers.unshift(20);
-console.log(numbers);
+// numbers.unshift(20);
+// console.log(numbers);
+
+// using constructor for arrays or objects
+// const name = new type(arguments);
+const objectiscarName = new Object(
+  (make = "Ford"),
+  (year = 2023),
+  (type = "Truck")
+);
+const arrayName = new Array(12, "number", "age", true);
+
+console.log(arrayName.push("emma"));
+console.log(arrayName[4]);
+console.log(objectiscarName);
